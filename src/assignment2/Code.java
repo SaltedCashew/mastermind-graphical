860870fed030d55323c0ee4d1dfe61841acc8977
SaltedCashew <bpgray@utexas.edu>
2014-09-24 14:46:56 -0500

    End of Lab commit

--- src/assignment2/Code.java
+++ src/assignment2/Code.java
@@ -1,6 +1,14 @@
 package assignment2;
 
+import java.util.ArrayList;
+
+
+
 public class Code
 {
-
+ int numPegs;
+ ArrayList<String> code;
+ ArrayList<String> color;
+	
+	
 }
--- src/assignment2/Game.java
+++ src/assignment2/Game.java
@@ -4,14 +4,16 @@
 */
 
 package assignment2;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 
 public class Game
 {
 	
 //	private int turnCounter;
-//	private ArrayList<String> codeHistory;
+	private ArrayList<String> codeHistory;
 	
 	
 	Game(boolean showCode){
@@ -44,12 +46,20 @@ public class Game
 		
 	}
 	
-	public static void Main(String[] args){
-		System.out.println("You have 12 guesses to figure out the secret code or you lose the game. Are you ready to play? /n(Y/N):");
+	public static void main(String[] args){
+		
 		Scanner inputScanner = new Scanner(System.in); 
+
+		System.out.println("You have 12 guesses to figure out the secret code or you lose the game. Are you ready to play? \n(Y/N):");
 		String playerInput = inputScanner.nextLine();
-		System.out.println(playerInput);
+		char c = playerInput.charAt(0);
 		inputScanner.close();
+		System.out.println(c);
+		if(c=='y' || c=='Y'){
+			Game masterMind = new Game(true);
+			masterMind.RunGame();
+		}
+		
 	}
 
 }
