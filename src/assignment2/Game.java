99b20effb69912cf3f8958db33ea3e01935ba8cb
SaltedCashew <bpgray@utexas.edu>
2014-09-24 13:55:44 -0500

    Init Commit

---
+++ src/assignment2/Game.java
@@ -0,0 +1,50 @@
+package assignment2;
+
+import assignment2.ArrayList;
+import assignment2.feedback;
+
+public class Game
+{
+	
+	private int turnCounter;
+	private ArrayList<String> codeHistory;
+	
+	
+	Game(boolean){
+		turnCounter = 12;
+	}
+	
+	void RunGame(void){
+		
+	}
+	
+	public void DisplayHistory(void){
+		
+	}
+	
+	String GenerateHistory(feedback, String){
+		
+	}
+	
+	void DisplayWinMsg(void){
+		
+	}
+	
+	void DisplayLoseMsg(void){
+		
+	}
+	
+	void DisplayPrompt(void){
+		
+	}
+
+}
+
+public class Code{
+	
+}
+
+public class Feedback{
+	
+	
+}
