c248a3fc35f29f512555ab9e4cb2af2fb1147cac
SaltedCashew <bpgray@utexas.edu>
2014-10-01 14:11:25 -0500

    Lab update

--- src/assignment2/A2Driver.java
+++ src/assignment2/A2Driver.java
@@ -5,6 +5,7 @@
 
 package assignment2;
 
+import java.io.PrintStream;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -13,6 +14,7 @@ public class A2Driver {
 	public static void main(String[] args){
 		
 		Scanner inputScanner = new Scanner(System.in);
+		InstPrompt();
 		System.out.println("You have 12 guesses to figure out the secret code or you lose the game.");
 		while(true) {
 			try{
@@ -62,5 +64,22 @@ public class A2Driver {
 		return spoil;
 	}
 	
+	private static void InstPrompt()
+	{
+		//PrintStream prompt = new PrintStream();
+		System.out.println("Welcome to MasterMind\n");
+		System.out.println("The computer will think of a secret code. \nThe code consists of 4 colored pegs. ");
+		System.out.println("The pegs MUST be one of six colors: blue, green, orange, purple, red, or yellow.");
+		System.out.println(" A color may appear more than once in the code. You try to guess what colored pegs are in the code and what order they are in. ");
+		System.out.println("After you make a valid guess the result (feedback) will be displayed.");
+		System.out.println("The result consists of a black peg for each peg you have guessed exactly correct (color and position) in your guess.  ");
+		System.out.println(" For each peg in the guess that is the correct color, but is out of position, you get a white peg.");
+		System.out.println(" For each peg, which is fully incorrect, you get no feedback.  ");
+		
+		System.out.println("Only the first letter of the color is displayed. B for Blue, R for Red, and so forth.  ");
+		System.out.println("When entering guesses you only need to enter the first character of each color as a capital letter.");
+		System.out.println("  ");
+		
+	}
 	
 }
